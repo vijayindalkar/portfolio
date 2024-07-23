cf5991d0d0c124c8c863e3dd4f84a10b0632e336
Vijay Indalkar <61607592+vijayindalkar@users.noreply.github.com>
2024-07-23 09:53:05 +0530

    added heroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import Spotlight from "@/components/HeroSection";
+import { FlipWords } from "@/components/ui/flip-words";
 
 export default function Home() {
+  const words = ["Hello", "World", "Example", "Words"];
   return (
     <div>
-      
+      <div className="h-[40rem] flex justify-center items-center px-4">
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+      <Spotlight />
+        Build
+        <FlipWords words={words} /> <br />
+        websites with Aceternity UI
+      </div>
+    </div>
     </div>
   );
 }
