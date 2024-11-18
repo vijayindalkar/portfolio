a8643cd7f76572b25c71479e2be52f516b7247dc
Vijay Indalkar <61607592+vijayindalkar@users.noreply.github.com>
2024-11-18 17:15:12 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,15 @@ export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m John</Heading>
+      <Heading className="font-black">Hello there! I&apos;m Vijay</Heading>
       <Paragraph className="max-w-xl mt-4">
         I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        <Highlight>building products</Highlight> and web apps that can <Highlight>Simplify , Streamline , Scale</Highlight> tasks
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a senior software engineer with{" "}
-        <Highlight>7 years of experience</Highlight> building scalable web apps
+        I&apos;m a  software engineer with{" "}
+        <Highlight>Close to 2 years of experience 🙂‍↕
+        </Highlight> building scalable web apps
         that are performance optimized and good looking.
       </Paragraph>
       <Heading
